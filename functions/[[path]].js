cdcac9573d61d8418045ece916dddfcd77f76290
shinnflove79 <shinnflove79@gmail.com>
2026-07-28 10:34:33 +0900

    Serve extensionless HTML routes in Pages Function

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -41,7 +41,8 @@ const buildLocalizedPath = (pathname, locale) => {
 }
 
 export async function onRequest(context) {
-  const url = new URL(context.request.url)
+  const request = context.request
+  const url = new URL(request.url)
   const normalizedPath = normalizePath(url.pathname)
   const queryLocale = normalizeLocaleQuery(url.searchParams.get('lang'))
 
@@ -54,6 +55,22 @@ export async function onRequest(context) {
   if (/^\/en(?:\/|$)/i.test(normalizedPath)) {
     url.pathname = stripLocalePrefix(normalizedPath)
     return Response.redirect(url.toString(), 301)
+  }
+
+  if (!hasFileExtension(normalizedPath) && normalizedPath !== '/' && url.pathname.endsWith('/')) {
+    url.pathname = normalizedPath
+    return Response.redirect(url.toString(), 308)
+  }
+
+  if ((request.method === 'GET' || request.method === 'HEAD') && !hasFileExtension(normalizedPath) && normalizedPath !== '/') {
+    const assetUrl = new URL(request.url)
+    assetUrl.pathname = `${normalizedPath}.html`
+
+    const assetResponse = await context.env.ASSETS.fetch(new Request(assetUrl.toString(), request))
+
+    if (assetResponse.ok) {
+      return assetResponse
+    }
   }
 
   return context.next()
